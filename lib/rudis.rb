2109aa4d25431b115da7c265d030c59e192daf8c
Jay Adkisson <jay@causes.com>
2010-06-10 18:43:52 -0700

    require the core_ext lib

--- lib/rudis.rb
+++ lib/rudis.rb
@@ -1,2 +1,15 @@
+#only once
+require begin
+  File.expand_path(
+    File.join(
+      File.dirname(__FILE__),
+      '..',
+      'vendor',
+      'core_ext',
+      'init'
+    )
+  )
+end
+
 class Rudis
 end
