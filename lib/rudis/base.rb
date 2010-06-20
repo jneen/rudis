3dad87b3901cf5aa51d1841a3b6b388aed79d98b
Jay Adkisson <jay@causes.com>
2010-06-20 15:54:21 -0700

    instance_key_base doesn't make sense anymore

--- lib/rudis/base.rb
+++ lib/rudis/base.rb
@@ -31,11 +31,6 @@ class Rudis
       def self.redis
         @redis ||= super
       end
-
-      attr_writer :instance_key_base
-      def self.instance_key_base
-        @instance_key_base ||= []
-      end
     end
 
     def redis
